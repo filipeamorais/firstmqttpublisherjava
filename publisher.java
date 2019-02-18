f6ae33a56ca59653259919f2395fc469ff408773
Filipe de Andrade Morais <afilipem@gmail.com>
2019-02-18 17:48:41 -0600

    new: adding the code from the slides for the object instanting, method to publish and method to generate temperatures.

--- publisher.java
+++ publisher.java
@@ -6,10 +6,50 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import java.util.Random;
 import java.net.*;
 
+//declaring the public class
 public class Publisher {
+    //atributes
     public static final String BROKER_URL = "tcp://iot.eclipse.org:1883";
     //public static final String BROKER_URL = "tcp://localhost:1883";
-
     private MqttClient client;
-    String topic = "Demo Topic"
+    String topic = "Demo Topic";
+
+    //object instantiation
+    public Publisher(){
+        String clientId = getMacAddress() + "-pub";
+        System.out.println("Client ID="+clientId);
+            try{
+                client = new MqttClient (BROKER_URL, clientId);
+                MqttConnectOptions options = MqttConnectOptions();
+                options.setCleanSession(true);
+                final MqttTopic temperatureTopic = client.getTopic(topic);
+                options.setWill(temperatureTopic, "I'm gone".getBytes(), 2, true);
+                client.connect(options);
+            }catch (MqttException e){
+                e.printStackTrace();System.exit(1);
+            }
+        }
+
+    //call for starting publishing
+    public void startPublishing() throws MqttException{
+            try{
+                for(int i=0;i<10;i++)
+                publishTemperature();
+                Thread.sleep(2000);
+                for(int i = 0; i<10; i++)
+                    publishTemperature();
+                client.disconnect();
+            } catch (Exception e) {e.printStackTrace();}
+        }
+    }
+
+    //generate temperatures
+    private void publishTemperature() throws MqttException {
+        Random rand = new Random ();
+        final int tempVal = rand.nextInt(20) + 30;
+        final String temperature = tempVal + "°C";
+        MqttMessage message = new MqttMessage(temperature.getBytes());
+        client.publish(topic, message);
+        System.out.println("Publishing"+message);
+    }
 }
